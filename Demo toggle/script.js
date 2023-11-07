15a635f948b2182ed6271df0fe8b6bc2f454fa64
habeebrn <habeebrn.id@gmail.com>
2023-11-07 13:30:30 +0530

    Tuggle nav bar

---
+++ Demo toggle/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+const toggleBtn = document.getElementById("toggleBtn");
+const links = document.querySelector("links");
+
+toggleBtn.addEventListener("click", () => {
+ links.classList.toggle(".show-links")
+
+});
